4b106603384091c9e19c99574f071bc02433f92b
Sankalpa Sarkar <codbegtocodpro@gmail.com>
2022-08-17 09:58:13 +0530

    Organised Files

---
+++ srccode/src/Js codes/pagestyle.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Record from '../data.json';
+
+
+const pagestyle =(steps,ctr)=>{
+
+  return(
+    <div className={`paintings${ctr}`}>
+      {
+        steps.map((index)=>{
+          return(
+
+            <div className={`painting${ctr}`}>
+
+              <img src={Record[index].images.thumbnail} alt="" />
+
+              <div className="desc">
+
+                <h2 className="name">{Record[index].name}</h2>
+                <p className="artistnm1">{Record[index].artist.name}</p>
+
+              </div>
+
+            </div>
+
+          )
+        })
+      }
+
+    </div>
+  )
+}
+
+export default pagestyle
--- srccode/src/index.js
+++ srccode/src/index.js
@@ -1,9 +1,12 @@
 import React from'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+
+import './css styles/index.css';
+import App from './Js codes/App';
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
 <React.StrictMode>
     <App/>
